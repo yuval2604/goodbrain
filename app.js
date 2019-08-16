3c623f304a5dd07e4c1f90077d0a3af5894fb70f
yuval Beiser <yuvalbeiser@yuvals-MacBook-Pro.local>
2019-08-16 13:10:48 +0300

    first commit

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    passport    = require("passport"),
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
     LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
-    Exercise  = require("./models/exercise"),
-    Comment     = require("./models/comment"),
-    User        = require("./models/user"),
-    seedDB      = require("./seeds")
+    Exercise = require("./models/exercise"),
+    Comment = require("./models/comment"),
+    User = require("./models/user"),
+    seedDB = require("./seeds")
 
 //requring routes    
-var commentRoutes= require("./routes/comments"),
-    exerciseRoutes= require("./routes/exercises"),
+var commentRoutes = require("./routes/comments"),
+    exerciseRoutes = require("./routes/exercises"),
     indexRoutes = require("./routes/index");
-    
+
 //mongoose.connect("mongodb://localhost/Goodbrain");
 
-mongoose.connect("mongodb://Yuval:yuval2604@ds143262.mlab.com:43262/goodbrain");
+mongoose.connect("mongodb://Yuval:yuval2604@ds233596.mlab.com:33596/goodbrain");
 
- // mongodb://yuval:yuval2604@ds143262.mlab.com:43262/goodbrain
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
@@ -40,18 +38,25 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   next();
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
 });
 
 app.use("/", indexRoutes);
 app.use("/exercises", exerciseRoutes);
 app.use("/exercises/:id/comments", commentRoutes);
 
+// app.get("/step", function(req, res){
+//     res.render("StepUp/index.ejs");
+// });
 
-
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The GoodBrain Server Has Started!");
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("The GoodBrain Server Has Started!");
 });
 
+
+// mkdir data
+// echo 'mongod --bind_ip=$IP --dbpath=data --nojournal --rest "$@"' > mongod
+// chmod a+x mongod
+// ./mongod
--- seeds.js
+++ seeds.js
@@ -5,54 +5,59 @@ var Comment   = require("./models/comment");
 var data = [
     {
         name: "Cloud's Rest", 
-        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        image: "https://images.unsplash.com/photo-1545624770-2f7822d4601f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+    },
+    {
+        name: "Cloud's Rest", 
+        image: "https://images.unsplash.com/photo-1545624770-2f7822d4601f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit"
     },
     {
         name: "Desert Mesa", 
-        image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        image: "https://images.unsplash.com/photo-1545559054-8f4f9e855781?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit"   
     },
     {
         name: "Canyon Floor", 
-        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        image: "https://images.unsplash.com/photo-1545621502-58d330faebb1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit"
     }
 ]
 
 function seedDB(){
-   //Remove all exercises
-//   Exercise.remove({}, function(err){
-        // if(err){
-        //     console.log(err);
-        // }
-        // console.log("removed exercise!");
-        //  //add a few exercises
-        // data.forEach(function(seed){
-        //     Exercise.create(seed, function(err, exercise){
-        //         if(err){
-        //             console.log(err)
-        //         } else {
-        //             console.log("added a exercise");
-        //             //create a comment
-        //             Comment.create(
-        //                 {
-        //                     text: "This place is great, but I wish there was internet",
-        //                     author: "Homer"
-        //                 }, function(err, comment){
-        //                     if(err){
-        //                         console.log(err);
-        //                     } else {
-        //                         exercise.comments.push(comment);
-        //                         exercise.save();
-        //                         console.log("Created new comment");
-        //                     }
-        //                 });
-        //         }
-        //     });
-        // });
-   // }); 
-    //add a few comments
+ // Remove all exercises
+  Exercise.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed exercise!");
+         //add a few exercises
+        data.forEach(function(seed){
+            Exercise.create(seed, function(err, exercise){
+                if(err){
+                    console.log(err)
+                } else {
+                    console.log("added a exercise");
+                    //create a comment
+                    Comment.create(
+                        {
+                            text: "This place is great, but I wish there was internet",
+                            author: "Homer"
+                        }, function(err, comment){
+                            if(err){
+                                console.log(err);
+                            } else {
+                                exercise.comments.push(comment);
+                                exercise.save();
+                                console.log("Created new comment");
+                            }
+                        });
+                }
+            });
+        });
+   }); 
+  //  add a few comments
 }
 
 module.exports = seedDB;
