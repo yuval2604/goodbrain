9eb073f5336ea70c2692a3f6e5aac2fb2455d5d0
yuval Beiser <yuvalbeiser@yuvals-MacBook-Pro.local>
2019-08-16 13:28:48 +0300

    first commit

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 var express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose"),
-    passport = require("passport"),
-    LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override"),
-    Exercise = require("./models/exercise"),
-    Comment = require("./models/comment"),
-    User = require("./models/user"),
-    seedDB = require("./seeds")
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  Exercise = require("./models/exercise"),
+  Comment = require("./models/comment"),
+  User = require("./models/user"),
+  seedDB = require("./seeds");
 
-//requring routes    
+//requring routes
 var commentRoutes = require("./routes/comments"),
-    exerciseRoutes = require("./routes/exercises"),
-    indexRoutes = require("./routes/index");
+  exerciseRoutes = require("./routes/exercises"),
+  indexRoutes = require("./routes/index");
 
 //mongoose.connect("mongodb://localhost/Goodbrain");
 
-mongoose.connect("mongodb://Yuval:yuval2604@ds233596.mlab.com:33596/goodbrain");
+mongoose.connect("mongodb://mac:Aa1234@ds233596.mlab.com:33596/goodbrain");
+//mongodb://StepUp:Yuval2604@ds233596.mlab.com:33596/goodbrain
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -25,13 +26,14 @@ app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 seedDB();
 
-
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "Once again Rusty wins cutest dog!",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -39,8 +41,8 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next) {
-    res.locals.currentUser = req.user;
-    next();
+  res.locals.currentUser = req.user;
+  next();
 });
 
 app.use("/", indexRoutes);
@@ -51,10 +53,10 @@ app.use("/exercises/:id/comments", commentRoutes);
 //     res.render("StepUp/index.ejs");
 // });
 
-app.listen(process.env.PORT, process.env.IP, function() {
-    console.log("The GoodBrain Server Has Started!");
+console.log(process.env.PORT);
+app.listen(8000, process.env.IP, function() {
+  console.log("The GoodBrain Server Has Started!");
 });
-
 
 // mkdir data
 // echo 'mongod --bind_ip=$IP --dbpath=data --nojournal --rest "$@"' > mongod
